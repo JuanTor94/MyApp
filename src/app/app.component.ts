50698548df62d9b0698e4c56992f8d630df31b2c
Juan Torres <torres70@misena.edu.co>
2020-08-14 22:11:52 -0500

    Se creó el hola mundo en angular 10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'MyApp';
+
+export class AppComponent{
+  world = 'Hola mundo en Angular';
 }
